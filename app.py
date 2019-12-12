d702b8d2d07618c2dccb9e1af38c3635656f5260
myname21-meet <myname21@meet.mit.edu>
2019-12-12 12:01:45 -0500

    list is crapped

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import json, requests
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,16 +9,29 @@ def home():
 
 @app.route('/study_image', methods = ['POST'])
 def study_image():
-    
-    image_url = request.form['url-input']
-    # At this point you have the image_url value from the front end
-    # Your job now is to send this information to the Clarifai API
-    # and read the result, make sure that you read and understand the
-    # example we covered in the slides! 
+	headers = {'Authorization': 'Key 814705368f074d60b8604409fd6a3f9d'}
 
-    # YOUR CODE HERE!
-    
-    return render_template('home.html', results="No results yet :(")
+	api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+
+	image_url = request.form['url-input']
+	
+	data ={"inputs": [
+	{
+	"data": {
+		"image": {
+			"url": image_url
+		}
+	}
+	}
+	]}
+
+	response = requests.post(api_url, headers=headers, data=json.dumps(data))
+	
+	response_dict = json.loads(response.content)
+
+	results = response_dict["outputs"][0]["data"]["concepts"]#[0]["name"]
+
+	return render_template('data.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
